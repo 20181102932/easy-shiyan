3254fc562ed02e8b7c2c7337aed86b1a4a3aa80c
20181102932 <908907990@qq.com>
2019-06-20 10:47:30 +0800

    yangli
    zhongyao

--- easy shiyan/main.cpp
+++ easy shiyan/main.cpp
@@ -1,15 +1,18 @@
-//
-//  main.cpp
-//  easy shiyan
-//
-//  Created by s20181102932 on 2019/6/20.
-//  Copyright © 2019 s20181102932. All rights reserved.
-//
-
-#include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+#include<iostream>
+#include<fstream>
+using namespace std;
+int main()
+{
+    int a,b;
+    ifstream in("/Users/s20181102932/Desktop/123.txt");
+        in>>a>>b;
+    ofstream out("/Users/s20181102932/Desktop/321.txt");
+    {
+        if(out.is_open())
+        {
+            out<<a<<" "<<b+1;
+            out.close();
+        }
+    }
     return 0;
 }
